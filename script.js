f61ee270b56693fad22b9bbb187c27a3dc352bbb
EmeraldXD <102572375+EmeraldXD@users.noreply.github.com>
2023-09-14 11:51:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -11,37 +11,47 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     generateButton.addEventListener("click", () => {
-        // Generate a random number between 1 and 25 to determine if it's shiny
-        const isShiny = Math.random() <= 1 / 25;
+        const isShiny = Math.random() <= 3.0864 / 25;
 
-        // Generate a random Pokémon ID between 1 and 1015
-        const randomPokemonId = Math.floor(Math.random() * 1014) + 1;
+        const randomPokemonId = Math.ceil(Math.random() * 1014) + 1;
 
-        // URL of the PokeAPI for the random Pokémon
         const apiUrl = `https://pokeapi.co/api/v2/pokemon/${randomPokemonId}/`;
 
         fetch(apiUrl)
             .then((response) => response.json())
             .then((data) => {
-                // Extract the Pokémon's name, base stats, and calculate the total base stat
                 const name = data.name;
                 const stats = data.stats;
                 const baseStatTotalValue = stats.reduce((total, stat) => total + stat.base_stat, 0);
 
-                // URL to the sprite of the Pokémon (shiny or regular)
                 const spriteUrl = isShiny ? data.sprites.front_shiny : data.sprites.front_default;
 
-                // Add "Shiny" to the name if it's a shiny Pokémon
-                const modifiedPokemonName = isShiny ? `Shiny ${capitalizeFirstLetter(name)}` : capitalizeFirstLetter(name);
+                const modifiedPokemonName = isShiny ? `${capitalizeFirstLetter(name)}⭐` : capitalizeFirstLetter(name);
 
-                // Update the HTML elements with Pokémon data
                 pokemonName.textContent = modifiedPokemonName;
                 baseStatTotal.textContent = `Base Stat Total: ${baseStatTotalValue}`;
                 statList.innerHTML = "";
                 stats.forEach((stat) => {
                     const statItem = document.createElement("li");
-                    statItem.textContent = `${stat.stat.name}: ${stat.base_stat}`;
-                    statList.appendChild(statItem);
+					const statName = stat.stat.name;
+					if (statName != "hp" && statName[7] != "-"){
+						const statNameEnding = stat.stat.name.toString().slice(1);
+	                    statItem.textContent = (statName.toString().charAt(0).toUpperCase() + statNameEnding + ": " + `${stat.base_stat}`);
+	                    statList.appendChild(statItem);	
+					}
+					else if (statName[7] != "-"){
+						statItem.textContent = ("HP: " + `${stat.base_stat}`);
+						statList.appendChild(statItem);	
+					}
+					else if(statName == "special-attack"){
+						statItem.textContent = ("Special Attack: " + `${stat.base_stat}`);
+						statList.appendChild(statItem);	
+					}
+					else {
+						statItem.textContent = ("Special Defense: " + `${stat.base_stat}`);
+						statList.appendChild(statItem);	
+					}
+					
                 });
 
                 pokemonSprite.src = spriteUrl;
