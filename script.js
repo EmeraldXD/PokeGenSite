8d636c9f07fc769256b35bd5342eb186e3b35571
EmeraldXD <102572375+EmeraldXD@users.noreply.github.com>
2023-09-14 19:44:35 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     generateButton.addEventListener("click", () => {
-        const isShiny = Math.random() <= 3.0864 / 25;
+        const isShiny = Math.random() <= 3.14 / 25;
 
         const randomPokemonId = Math.ceil(Math.random() * 1014) + 1;
 
@@ -34,22 +34,71 @@ document.addEventListener("DOMContentLoaded", () => {
                 stats.forEach((stat) => {
                     const statItem = document.createElement("li");
 					const statName = stat.stat.name;
+                    const statNum = Number(stat.base_stat);
 					if (statName != "hp" && statName[7] != "-"){
 						const statNameEnding = stat.stat.name.toString().slice(1);
 	                    statItem.textContent = (statName.toString().charAt(0).toUpperCase() + statNameEnding + ": " + `${stat.base_stat}`);
-	                    statList.appendChild(statItem);	
+	                    statList.appendChild(statItem);
+                        if (statNum <= 60){
+                            statItem.style.color = "#c91800";
+                        }
+                        else if (statNum > 60 && statNum <= 85){
+                            statItem.style.color = "#c9a500";
+                        }
+                        else if (statNum > 85 && statNum <= 125){
+                            statItem.style.color = "#57c200";
+                        }
+                        else{
+                            statItem.style.color = "#00c9ab";
+                        }
 					}
 					else if (statName[7] != "-"){
 						statItem.textContent = ("HP: " + `${stat.base_stat}`);
 						statList.appendChild(statItem);	
+                        if (statNum <= 60){
+                            statItem.style.color = "#c91800";
+                        }
+                        else if (statNum > 60 && statNum <= 85){
+                            statItem.style.color = "#c9a500";
+                        }
+                        else if (statNum > 85 && statNum <= 125){
+                            statItem.style.color = "#57c200";
+                        }
+                        else{
+                            statItem.style.color = "#00c9ab";
+                        }
 					}
 					else if(statName == "special-attack"){
 						statItem.textContent = ("Special Attack: " + `${stat.base_stat}`);
 						statList.appendChild(statItem);	
+                        if (statNum <= 60){
+                            statItem.style.color = "#c91800";
+                        }
+                        else if (statNum > 60 && statNum <= 85){
+                            statItem.style.color = "#c9a500";
+                        }
+                        else if (statNum > 85 && statNum <= 125){
+                            statItem.style.color = "#57c200";
+                        }
+                        else{
+                            statItem.style.color = "#00c9ab";
+                        }
 					}
 					else {
 						statItem.textContent = ("Special Defense: " + `${stat.base_stat}`);
-						statList.appendChild(statItem);	
+						statList.appendChild(statItem);
+                        if (statNum <= 60){
+                            statItem.style.color = "#c91800";
+                        }
+                        else if (statNum > 60 && statNum <= 85){
+                            statItem.style.color = "#c9a500";
+                        }
+                        else if (statNum > 85 && statNum <= 125){
+                            statItem.style.color = "#57c200";
+                        }
+                        else{
+                            statItem.style.color = "#00c9ab";
+                        }
 					}
 					
                 });
